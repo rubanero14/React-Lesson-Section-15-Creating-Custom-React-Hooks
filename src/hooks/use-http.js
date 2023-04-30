9ab8cf40f040ff2638acd8f15db55d78efb67ec4
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-05-01 03:08:15 +0800

    Using the Custom Http Hook

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useHttp = (processData, requestConfig) => {
+const useHttp = (processData, requestConfig = null) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -11,6 +11,12 @@ const useHttp = (processData, requestConfig) => {
       const response = await fetch(
         "https://react-certification-68c63-default-rtdb.firebaseio.com/tasks.json",
         requestConfig
+          ? {
+              method: requestConfig.method,
+              body: JSON.stringify(requestConfig.body),
+              headers: requestConfig.headers,
+            }
+          : null
       );
 
       if (!response.ok) {
@@ -19,6 +25,8 @@ const useHttp = (processData, requestConfig) => {
 
       const data = await response.json();
 
+      
+      
       processData(data);
     } catch (err) {
       setError(err.message || "Something went wrong!");
