ec8cef3ef3f58bb70da635e13d6931791484a599
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-05-02 23:10:49 +0800

    final touchup

--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,6 +1,6 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
-import classes from './TaskForm.module.css';
+import classes from "./TaskForm.module.css";
 
 const TaskForm = (props) => {
   const taskInputRef = useRef();
@@ -17,8 +17,14 @@ const TaskForm = (props) => {
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
-      <input type='text' ref={taskInputRef} />
-      <button>{props.loading ? 'Sending...' : 'Add Task'}</button>
+      <input type="text" ref={taskInputRef} />
+      <button className="btn">
+        {!props.loading && <i class="bi bi-plus-circle"></i>}
+        {props.loading ? "Sending..." : " Add Task"}
+      </button>
+      <a className="btn" href="/" target="_blank">
+        <i class="bi bi-filetype-jsx"></i> Source Code
+      </a>
     </form>
   );
 };
